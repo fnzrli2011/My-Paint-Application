a660c5d5a01d90e67362d9e0c4f234da61cb5160
fnzrli2011 <fnzrli109@gmail.com>
2024-07-02 14:19:43 +0400

    My first C# application

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(841, 419);
+            Name = "Form1";
+            Text = "Boyama";
+            Load += Form1_Load;
+            QueryAccessibilityHelp += Form1_QueryAccessibilityHelp;
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp_1;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        int control = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+        private void Form1_QueryAccessibilityHelp(object sender, QueryAccessibilityHelpEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (control == 1)
+            {
+                
+                g.FillEllipse(Brushes.Red, e.X, e.Y, 10, 10);
+            }
+        }
+
+
+        private void Form1_MouseUp_1(object sender, MouseEventArgs e)
+        {
+            control = 0;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            control = 1;
+        }
+    }
+}
